3a3f1a712226a1a65b4ca4b5a77bea591f912882
romanmuni8 <romanmuni8@gmail.com>
2023-02-13 07:09:27 -0500

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Roman on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
